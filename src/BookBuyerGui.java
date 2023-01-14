97682718e8948a320e91a0b46a68c89a947bf350
igor <igorniemiec00@gmail.com>
2023-01-14 17:08:30 +0100

    Init

---
+++ src/BookBuyerGui.java
@@ -0,0 +1,57 @@
+package jadelab2;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+class BookBuyerGui extends JFrame {	
+	private BookBuyerAgent myAgent;
+	
+	private JTextField titleField;
+	
+	BookBuyerGui(BookBuyerAgent a) {
+		super(a.getLocalName());
+		
+		myAgent = a;
+		
+		JPanel p = new JPanel();
+		p.setLayout(new GridLayout(2, 2));
+		p.add(new JLabel("Title:"));
+		titleField = new JTextField(15);
+		p.add(titleField);
+		getContentPane().add(p, BorderLayout.CENTER);
+		
+		JButton addButton = new JButton("Search");
+		addButton.addActionListener(ev -> {
+			try {
+				String title = titleField.getText().trim();
+				myAgent.lookForTitle(title);
+				titleField.setText("");
+			}
+			catch (Exception e) {
+				JOptionPane.showMessageDialog(BookBuyerGui.this, "Invalid values. " + e.getMessage(), "Error", JOptionPane.ERROR_MESSAGE);
+			}
+		});
+		p = new JPanel();
+		p.add(addButton);
+		getContentPane().add(p, BorderLayout.SOUTH);
+		
+		addWindowListener(new	WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+				myAgent.doDelete();
+			}
+		} );
+		
+		setResizable(false);
+	}
+	
+	public void display() {
+		pack();
+		Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+		int centerX = (int)screenSize.getWidth() / 2;
+		int centerY = (int)screenSize.getHeight() / 2;
+		setLocation(centerX - getWidth() / 2, centerY - getHeight() / 2);
+		setVisible(true);
+	}	
+}
